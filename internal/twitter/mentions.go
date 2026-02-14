6b49c1407121538c85f58fa2cd11915b65d19abb
Joao Victor Sena <jvsena16@gmail.com>
2026-02-14 15:04:00 -0300

    fix: remoce sinceID parameter

--- internal/twitter/mentions.go
+++ internal/twitter/mentions.go
@@ -5,13 +5,13 @@ import (
 	"fmt"
 )
 
-func (c *Client) GetMentions(sinceID string) (*TweetsResponse, error) {
+func (c *Client) GetMentions() (*TweetsResponse, error) {
 	params := map[string]string{
 		"tweet.fields": "author_id,text,created_at,conversation_id,in_reply_to_user_id",
 		"expansions":   "author_id",
 	}
-	if sinceID != "" {
-		params["since_id"] = sinceID
+	if c.SinceID != "" {
+		params["since_id"] = c.SinceID
 	}
 	respBytes, err := c.doGet(fmt.Sprintf("/2/users/%s/mentions", c.BotUserID), params)
 	if err != nil {
