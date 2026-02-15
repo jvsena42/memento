3f140cf8919b3d64f083f017147303942d8dbaf4
Joao Victor Sena <jvsena16@gmail.com>
2026-02-15 16:52:30 -0300

    chore: get target tweet

--- internal/bot/handler.go
+++ internal/bot/handler.go
@@ -1,6 +1,8 @@
 package bot
 
 import (
+	"fmt"
+
 	"github.com/jvsena42/memento/internal/config"
 	"github.com/jvsena42/memento/internal/storage"
 	"github.com/jvsena42/memento/internal/twitter"
@@ -10,4 +12,21 @@ type Handler struct {
 	Client       *twitter.Client
 	CapsuleStore *storage.CapsuleStore
 	Config       *config.Config
+}
+
+func (h *Handler) ProcessMention(mention twitter.Tweet, users []twitter.User) error {
+
+	var targetTweet *twitter.TweetResponse
+	var err error
+
+	if mention.InReplyToUserID != nil {
+		targetTweet, err = h.Client.GetTweet(mention.ConversationID)
+	} else {
+		targetTweet, err = h.Client.GetTweet(mention.ID)
+	}
+
+	if err != nil {
+		return fmt.Errorf("failed to fetch target tweet: %w", err)
+	}
+
 }
