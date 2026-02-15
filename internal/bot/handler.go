4e73344a48146906763aefcaf1a9e5b1996ba8f8
Joao Victor Sena <jvsena16@gmail.com>
2026-02-15 17:45:09 -0300

    feat: save the capsule

--- internal/bot/handler.go
+++ internal/bot/handler.go
@@ -2,7 +2,7 @@ package bot
 
 import (
 	"fmt"
-	"os/user"
+	"time"
 
 	"github.com/jvsena42/memento/internal/config"
 	"github.com/jvsena42/memento/internal/storage"
@@ -34,21 +34,40 @@ func (h *Handler) ProcessMention(mention twitter.Tweet, users []twitter.User) er
 
 	requesterHandler := findUser(users, mention.AuthorID)
 
-	if h.CapsuleStore.TweetAlreadySaved(targetTweet.Tweet.ID) {
+	saved, err := h.CapsuleStore.TweetAlreadySaved(targetTweet.Tweet.ID)
+	if err != nil {
+		return fmt.Errorf("failed to check tweet: %w", err)
+	}
+	if saved {
 		h.Client.PostTweet("This one's already saved! ⏳", "", mention.ID)
 		return nil
 	}
 
-	if h.CapsuleStore.UserSavedToday()(mention.AuthorID) {
+	saved, err = h.CapsuleStore.UserSavedToday(mention.AuthorID)
+	if err != nil {
+		return fmt.Errorf("failed to check tweet: %w", err)
+	}
+	if saved {
 		h.Client.PostTweet("Come back tomorrow! 🕰️", "", mention.ID)
 		return nil
 	}
 
-	
+	capsule := storage.Capsule{
+		RequesterID:     mention.AuthorID,
+		RequesterHandle: requesterHandler,
+		TweetID:         targetTweet.Tweet.ID,
+		TweetAuthor:     tweetAuthor,
+		TweetText:       targetTweet.Tweet.Text,
+		IsReply:         mention.InReplyToUserID != nil,
+		RepublishAt:     time.Now().Add(h.Config.RepublishDelay),
+	}
+
+	h.CapsuleStore.Create(&capsule)
+	return nil
 }
 
 func findUser(users []twitter.User, userID string) string {
-	for _, user : = range users {
+	for _, user := range users {
 		if user.ID == userID {
 			return user.UserName
 		}
